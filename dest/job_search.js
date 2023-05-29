25bbd9e3a45d52027f20d66588ca2e4f9a85b6dd
lilpumpy19 <100938542+lilpumpy19@users.noreply.github.com>
2023-05-29 13:33:32 +0400

    01
    исправлено несколько ошибок

--- dest/job_search.js
+++ dest/job_search.js
@@ -1,31 +1,34 @@
 "use strict";
 // ��������� ����� � �������� ��� ����������� �����������
-console.log('j');
+// ��������� ����� � �������� ��� �����������
 const form = document.getElementById('jobSearchForm');
 const vacancyResults = document.getElementById('vacancyResults');
-// ���������� �������� �����
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // �������������� �������� �����
-    // ��������� �������� ����� �����
-    const jobTitle = document.getElementById('jobTitle').value;
-    const location = document.getElementById('location').value;
-    // ��������� ������� �������� �� LocalStorage
-    const vacancies = JSON.parse(localStorage.getItem('vacancies') || '[]');
-    // ���������� �������� �� �������� � ��������������
-    const filteredVacancies = vacancies.filter((vacancy) => {
-        return vacancy.jobTitle.toLowerCase().includes(jobTitle.toLowerCase()) &&
-            vacancy.location.toLowerCase().includes(location.toLowerCase());
+if (form && vacancyResults) {
+    // ���������� �������� �����
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // �������������� �������� �����
+        // ��������� �������� ����� �����
+        const jobTitle = document.getElementById('jobTitle').value;
+        const location = document.getElementById('location').value;
+        // ��������� ����������� �������� �� LocalStorage
+        const vacancies = JSON.parse(localStorage.getItem('vacancies') || '[]');
+        // ���������� �������� �� �������� � ��������������
+        const filteredVacancies = vacancies.filter((vacancy) => {
+            return vacancy.jobTitle.toLowerCase().includes(jobTitle.toLowerCase()) &&
+                vacancy.location.toLowerCase().includes(location.toLowerCase());
+        });
+        // ������� �������� ��� ����������� �����������
+        vacancyResults.innerHTML = '';
+        // ����������� ��������������� ��������
+        filteredVacancies.forEach((vacancy) => {
+            const vacancyCardTemplate = document.getElementById('vacancyCardTemplate');
+            const vacancyCard = vacancyCardTemplate.content.cloneNode(true);
+            vacancyCard.querySelector('.vacancy-title').textContent = vacancy.jobTitle;
+            vacancyCard.querySelector('.vacancy-salary').textContent = `Salary: ${vacancy.salary}`;
+            vacancyCard.querySelector('.vacancy-location').textContent = `Location: ${vacancy.location}`;
+            const vacancyImage = vacancyCard.querySelector('.vacancy-image');
+            vacancyImage.src = vacancy.img;
+            vacancyResults.appendChild(vacancyCard);
+        });
     });
-    // ������� �������� ��� ����������� �����������
-    vacancyResults.innerHTML = '';
-    // ����������� ��������������� ��������
-    filteredVacancies.forEach((vacancy) => {
-        const vacancyCardTemplate = document.getElementById('vacancyCardTemplate');
-        const vacancyCard = vacancyCardTemplate.content.cloneNode(true);
-        vacancyCard.querySelector('.vacancy-title').textContent = vacancy.jobTitle;
-        vacancyCard.querySelector('.vacancy-salary').textContent = `Salary: ${vacancy.salary}`;
-        vacancyCard.querySelector('.vacancy-location').textContent = `Location: ${vacancy.location}`;
-        vacancyCard.querySelector('.vacancy-image').src = vacancy.img;
-        vacancyResults.appendChild(vacancyCard);
-    });
-});
+}
--- src/js/employers.ts
+++ src/js/employers.ts
@@ -1,34 +1,36 @@
-// Получение формы и полей ввода
+
 console.log('e')
-const form = document.querySelector('form');
-const jobTitleInput = document.getElementById('jobTitle') as HTMLInputElement;
-const locationInput = document.getElementById('location') as HTMLInputElement;
-const descriptionInput = document.getElementById('description') as HTMLTextAreaElement;
-const imgInput = document.getElementById('img') as HTMLInputElement;
-const salaryInput = document.getElementById('salary') as HTMLInputElement;
+// Получение формы и полей ввода
+const form = document.querySelector('form'),
+    jobTitleInput = document.getElementById('jobTitle') as HTMLInputElement | null,
+    locationInput = document.getElementById('location') as HTMLInputElement | null,
+    descriptionInput = document.getElementById('description') as HTMLTextAreaElement | null,
+    imgInput = document.getElementById('img') as HTMLInputElement | null,
+    salaryInput = document.getElementById('salary') as HTMLInputElement | null;
+if (form && jobTitleInput && locationInput && descriptionInput && imgInput && salaryInput) {
+    // Обработчик отправки формы
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Предотвращение отправки формы
 
-// Обработчик отправки формы
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Предотвращение отправки формы
+        // Создание объекта вакансии
+        const vacancy = {
+            jobTitle: jobTitleInput.value,
+            location: locationInput.value,
+            description: descriptionInput.value,
+            img: imgInput.value,
+            salary: salaryInput.value,
+        };
 
-    // Создание объекта вакансии
-    const vacancy = {
-        jobTitle: jobTitleInput.value,
-        location: locationInput.value,
-        description: descriptionInput.value,
-        img: imgInput.value,
-        salary: salaryInput.value,
-    };
+        // Получение текущего массива вакансий из LocalStorage или создание нового пустого массива
+        const vacancies = JSON.parse(localStorage.getItem('vacancies') || '[]');
 
-    // Получение текущего массива вакансий из LocalStorage или создание нового пустого массива
-    const vacancies = JSON.parse(localStorage.getItem('vacancies') || '[]');
+        // Добавление новой вакансии в массив
+        vacancies.push(vacancy);
 
-    // Добавление новой вакансии в массив
-    vacancies.push(vacancy);
+        // Сохранение обновленного массива вакансий в LocalStorage
+        localStorage.setItem('vacancies', JSON.stringify(vacancies));
 
-    // Сохранение обновленного массива вакансий в LocalStorage
-    localStorage.setItem('vacancies', JSON.stringify(vacancies));
-
-    // Очистка полей ввода после добавления вакансии
-    form.reset();
-});
+        // Очистка полей ввода после добавления вакансии
+        form.reset();
+    });
+}
